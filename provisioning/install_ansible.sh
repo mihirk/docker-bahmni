578b80a72b4070e936df29f1ea6935c1f8ed050f
mihirk <mihir.khatwani@gmail.com>
2014-09-16 23:56:11 +0530

    Adding tomcat role

--- provisioning/install_ansible.sh
+++ provisioning/install_ansible.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum install -y ansible
+if ! type "ansible-playbook" > /dev/null; then
+  yum install -y ansible
+fi
